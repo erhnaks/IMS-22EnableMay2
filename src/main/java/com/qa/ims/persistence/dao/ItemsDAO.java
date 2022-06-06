d4d1a50fdc341b974496102abda6d074734e1d0c
erhnaks <erhnaks@hotmail.com>
2022-06-06 17:14:57 +0100

    QPI-10 #comment Modified Items Controller and ItemsDAO

---
+++ src/main/java/com/qa/ims/persistence/dao/ItemsDAO.java
@@ -0,0 +1,98 @@
+package com.qa.ims.persistence.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Items;
+import com.qa.ims.utils.DBUtils;
+
+public class ItemsDAO implements Dao<Items> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public Items modelFromResultSet(ResultSet resultSet) throws SQLException {
+		Long item_id = resultSet.getLong("item_id");
+		String item_name = resultSet.getString("item_name");
+		float price = resultSet.getFloat("price");
+		return new Items(item_id, item_name, price);
+	}
+
+	// Reads all the items from the database and return items
+
+	@Override
+	public List<Items> readAll() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items");) {
+			List<Items> items = new ArrayList<>();
+			while (resultSet.next()) {
+				items.add(modelFromResultSet(resultSet));
+			}
+			return items;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return new ArrayList<>();
+
+	}// End of readAll Items from database
+
+	public Items readLatest() {
+
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM items ORDER BY item_id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public Items create(Items items) {
+
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO items(item_name, price) VALUES (?, ?)");) {
+			statement.setString(1, items.getItem_name());
+			statement.setFloat(2, items.getPrice());
+			statement.executeUpdate();
+			return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+
+	@Override
+	public Items read(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Items update(Items t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
