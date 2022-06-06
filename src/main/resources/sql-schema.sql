b21b0144406002b43953ee04bae570fab93e59a7
erhnaks <erhnaks@hotmail.com>
2022-06-06 15:42:26 +0100

    QPI-9 #comment modified .gitignore file and sql-schmea.sql has been modified

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,27 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+   `item_id` INT NOT NULL AUTO_INCREMENT UNIQUE,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `price` FLOAT DEFAULT NULL,
+    PRIMARY KEY (`item_id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+`orders_id` INT(11) NOT NULL AUTO_INCREMENT UNIQUE,
+`customer_id` INT(11) NOT NULL,
+PRIMARY KEY (`orders_id`),
+FOREIGN KEY(`customer_id`) REFERENCES `ims`.`customers`(id)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
+order_items_id INT(11) PRIMARY KEY AUTO_INCREMENT UNIQUE,
+orders_id INT(11) NOT NULL,
+item_id INT(11) NOT NULL,
+`quantity` INT NOT NULL,
+`item_price` FLOAT NOT NULL, 
+FOREIGN KEY (orders_id) REFERENCES `ims`.`orders`(orders_id),
+FOREIGN KEY (item_id) REFERENCES `ims`.`items`(item_id)
+);
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE IF NOT EXISTS `customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+   `item_id` INT NOT NULL AUTO_INCREMENT,
+    `item_name` VARCHAR(40) DEFAULT NULL,
+    `price` FLOAT DEFAULT NULL,
+    PRIMARY KEY (`item_id`)
+);
