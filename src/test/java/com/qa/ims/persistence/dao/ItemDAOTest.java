b518855202a79a19a7f78af7b6f9d95a96c709ad
erhnaks <erhnaks@hotmail.com>
2022-06-08 16:24:30 +0100

    QPI-20 #comment ItemDAOTest test has been writen and tested

---
+++ src/test/java/com/qa/ims/persistence/dao/ItemDAOTest.java
@@ -0,0 +1,61 @@
+package com.qa.ims.persistence.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.DBUtils;
+
+public class ItemDAOTest {
+
+	private final ItemDAO DAO = new ItemDAO();
+
+	@Before
+	public void setup() {
+		DBUtils.connect();
+		DBUtils.getInstance().init("src/test/resources/sql-schema.sql", "src/test/resources/sql-data.sql");
+	}
+
+	@Test
+	public void testCreate() {
+		final Item created = new Item(2L, "xxx", 1.09F);
+		assertEquals(created, DAO.create(created));
+	}
+
+	@Test
+	public void testReadAll() {
+		List<Item> expected = new ArrayList<>();
+		expected.add(new Item(1L, "cookie", 1.99F));
+		assertEquals(expected, DAO.readAll());
+
+	}
+
+	@Test
+	public void testReadLatest() {
+		assertEquals(new Item(1L, "cookie", 1.99f), DAO.readLatest());
+	}
+
+	@Test
+	public void testRead() {
+		final long ID = 1L;
+		assertEquals(new Item(ID, "cookie", 1.99F), DAO.read(ID));
+	}
+
+	@Test
+	public void testUpdate() {
+		final Item updated = new Item(1L, "crips", 1.49F);
+		assertEquals(updated, DAO.update(updated));
+
+	}
+
+	@Test
+	public void testDelete() {
+		assertEquals(1, DAO.delete(1));
+	}
+
+}
