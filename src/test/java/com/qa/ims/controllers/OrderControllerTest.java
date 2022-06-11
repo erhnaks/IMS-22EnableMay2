fc51b8db04e484534fcd73e1caad7ae5f70abdda
erhnaks <erhnaks@hotmail.com>
2022-06-11 19:09:08 +0100

    Test modified to cover high percentage

--- src/test/java/com/qa/ims/controllers/OrderControllerTest.java
+++ src/test/java/com/qa/ims/controllers/OrderControllerTest.java
@@ -65,33 +65,43 @@ public class OrderControllerTest {
 
 	@Test
 	public void testUpdate() {
-		Order updated = new Order(1L, 1L, 1);
-		
-		//Mockito.when(this.utils.getLong()).thenReturn(1L);
+		Order updated = new Order(1L, 1L, 1L, 1);
+
 		Mockito.when(this.utils.getLong()).thenReturn(1L, updated.getCustomerId(), updated.getItemId());
-		//Mockito.when(this.utils.getFloat()).thenReturn(1.99F);
 		Mockito.when(this.utils.getInteger()).thenReturn(1);
 		Mockito.when(this.dao.update(updated)).thenReturn(updated);
 
 		assertEquals(updated, this.controller.update());
 
 		Mockito.verify(this.utils, Mockito.times(3)).getLong();
-		//Mockito.verify(this.utils, Mockito.times(1)).getFloat();
 		Mockito.verify(this.utils, Mockito.times(1)).getInteger();
 		Mockito.verify(this.dao, Mockito.times(1)).update(updated);
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	@Test
+	public void testDelete() {
+		final long ID = 1L;
+
+		Mockito.when(utils.getLong()).thenReturn(ID);
+		Mockito.when(dao.delete(ID)).thenReturn(1);
+
+		assertEquals(1L, this.controller.delete());
+
+		Mockito.verify(utils, Mockito.times(1)).getLong();
+		Mockito.verify(dao, Mockito.times(1)).delete(ID);
+	}
+
 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
