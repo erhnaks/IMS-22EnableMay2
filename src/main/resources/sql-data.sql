c34fe45a384c96991d5dd64cca3d52cfc0244f47
erhnaks <erhnaks@hotmail.com>
2022-06-11 22:20:48 +0100

    Added dummy data to the source data,sql

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -6,4 +6,9 @@ INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Leonardo', 'Bee
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Erhan', 'Aksu');
 
 
-
+INSERT INTO `items` (`item_name`, `price`) VALUES ('water', 1.10);
+INSERT INTO `items` (`item_name`, `price`) VALUES ('juice', 2.50);
+INSERT INTO `items` (`item_name`, `price`) VALUES ('crisps', 1.99);
+INSERT INTO `items` (`item_name`, `price`) VALUES ('coke', 1.89);
+INSERT INTO `items` (`item_name`, `price`) VALUES ('bread', 1.49);
+INSERT INTO `items` (`item_name`, `price`) VALUES ('milk', 1.69);
